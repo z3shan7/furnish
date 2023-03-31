8188b04379340a675b653bdf9a3948860a0920ab
Shoaib Zeshan <szeeshan707@gmail.com>
2023-04-01 03:21:34 +0530

    building project section

--- app.js
+++ app.js
@@ -18,3 +18,43 @@ menuBtn.addEventListener('click', () => {
     list.classList.toggle('show-links')
 
 })
+
+const counters = document.querySelectorAll('.countUp__numbers')
+
+let speed = 200;
+
+// counters.forEach((counter, index) => {
+
+//     function updateNumber() {
+
+//         const targerNumber = counter.dataset.target;
+//         const intialNumber = counter.innerText;
+//         const incPerCount = targerNumber / speed;
+//         if (intialNumber < targerNumber) {
+//             counter.textContent = Math.floor(intialNumber + incPerCount)
+//             setTimeout(updateNumber, 40)
+//         }
+
+//     }
+
+//     updateNumber()
+
+// })
+
+let interval = 1;
+
+counters.forEach((counter) => {
+    let starValue = 0;
+    let endValue = counter.getAttribute('data-target')
+
+    let duration = Math.floor(interval / endValue)
+
+    let count = setInterval(function () {
+        starValue += 1;
+        counter.textContent = starValue;
+        if (starValue == endValue) {
+            clearInterval(count)
+        }
+
+    }, 1)
+})
