fec42a1395252b4be4406ba2577c59db31324513
Shoaib Zeshan <szeeshan707@gmail.com>
2023-04-08 00:56:18 +0530

    creating testimonial section

--- app.js
+++ app.js
@@ -58,3 +58,14 @@ counters.forEach((counter) => {
 
     }, 1)
 })
+
+// 
+
+var swiper = new Swiper(".testimonial__slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
